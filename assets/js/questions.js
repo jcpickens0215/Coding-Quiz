5e4e3e1f4e27eff0bb53bbd8704330af2a318fd6
Justin Pickens <justin.pickens@gmx.com>
2021-05-27 22:09:39 -0500

    Split the hard coded questions into a separate file, also added more comments and changed numCorrect into playerScore

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// This is an array storing question objects
+var listOfQuestions = [
+    {
+        "answer": "A",
+        "isTrueFalse": false,
+        "question": "The answer is A",
+        "options": ["A", "B", "C", "D"]
+    },
+    {
+        "answer": "B",
+        "isTrueFalse": false,
+        "question": "The answer is B",
+        "options": ["A", "B", "C", "D"]
+    },
+    {
+        "answer": "C",
+        "isTrueFalse": false,
+        "question": "The answer is C",
+        "options": ["A", "B", "C", "D"]
+    },
+    {
+        "answer": "D",
+        "isTrueFalse": false,
+        "question": "The answer is D",
+        "options": ["A", "B", "C", "D"]
+    },
+    {
+        "answer": "B",
+        "isTrueFalse": true,
+        "question": "True/False test, the answer is False",
+        "options": ["True", "False", "", ""]
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,68 +4,36 @@ var fldPlayField = document.querySelector("#playField");
 
 // Button Elements
 var btnStartButton = document.querySelector("#startButton");
-var btnSelectionA = document.querySelector("#selectionA");
-var btnSelectionB = document.querySelector("#selectionB");
-var btnSelectionC = document.querySelector("#selectionC");
-var btnSelectionD = document.querySelector("#selectionD");
+var btnSelectionA  = document.querySelector("#selectionA");
+var btnSelectionB  = document.querySelector("#selectionB");
+var btnSelectionC  = document.querySelector("#selectionC");
+var btnSelectionD  = document.querySelector("#selectionD");
 
 // Game variables
 var gameState = "GS_WELCOME"; // Useful for checking if we're on the welcome screen, game screen, or highscore screen
+
 var timeLeft = 60; // Total game time
-var numCorrect = 0; // Number of questions player answered correctly
+var playerScore = 0; // Store the player's score
 
-// I had to hardcode these questions because I couldn't figure out how to load a JSON
-var listOfQuestions = [
-    {
-        "answer": "A",
-        "isTrueFalse": false,
-        "question": "The answer is A",
-        "options": ["A", "B", "C", "D"]
-    },
-    {
-        "answer": "B",
-        "isTrueFalse": false,
-        "question": "The answer is B",
-        "options": ["A", "B", "C", "D"]
-    },
-    {
-        "answer": "C",
-        "isTrueFalse": false,
-        "question": "The answer is C",
-        "options": ["A", "B", "C", "D"]
-    },
-    {
-        "answer": "D",
-        "isTrueFalse": false,
-        "question": "The answer is D",
-        "options": ["A", "B", "C", "D"]
-    },
-    {
-        "answer": "B",
-        "isTrueFalse": true,
-        "question": "True/False test, the answer is False",
-        "options": ["True", "False", "", ""]
-    }
-];
-
-var currentQuestion = listOfQuestions[0]; // Variable to store current question
+var currentQuestion; // Variable to store current question
 var numQuestions = listOfQuestions.length; // Total number of questions
 var questionIndex = 0; // Which question are we on?
-var userSelectedAnswer = "";
+var userSelectedAnswer = ""; // Store the player's selected answer as a string
 
 /* INIT */
 // STEP 0
 // Function: Init
 function init() {
     gameState = "GS_WELCOME";
-    // Loads questions and highscores
-    // console.log(listOfQuestions);
-    // console.log(currentQuestion);
+    playerScore = 0;
+    questionIndex = 0;
+    currentQuestion = listOfQuestions[0];
 
     // Load highscores from system local storage
 
     // Clear Screen
     clearScreen();
+    renderWelcomeScreen();
 }
 
 /* RENDER */
@@ -75,43 +43,59 @@ function renderWelcomeScreen() {
     // fldTextField ==> print welcome/instructions
     fldTextField.children[0].textContent = "Welcome!";
     fldTextField.children[1].textContent = "Instructions: ";
+
     // fldGameText ==> display startButton
     btnStartButton.setAttribute("style", "visibility: visible; text-align:center;");
 }
 
-// RENDER
-// Function: Clear screen
+// Clears the screen of all text
 function clearScreen() {
+    // Clear fields
     fldTextField.children[0].textContent = "";
     fldTextField.children[1].textContent = "";
 
+    // Hide start button
     btnStartButton.setAttribute("style", "visibility: hidden;");
+
+    // Clear button text
     btnSelectionA.textContent = "";
     btnSelectionB.textContent = "";
     btnSelectionC.textContent = "";
     btnSelectionD.textContent = "";
 }
 
-// RENDER
-// Step 2b
-// Function: Render question and answer
+// Draw the question and answer selctions to the screen
 function renderQuestionAndAnswer(isTrueFalse) {
     // Print question to fldTextField
     fldTextField.children[0].textContent = "Question " + (questionIndex + 1);
     fldTextField.children[1].textContent = currentQuestion["question"];
+
+    // Fetch array of answer selections from key "options"
     var optionsArray = currentQuestion["options"];
 
-    // Print selection options to fldPlayField
+    ///// Print selection options to fldPlayField
+
+    // Make sure the unordered list is visible
     fldPlayField.children[1].setAttribute("style", "visibility: visible;");
+
+    // Hide the start button
     btnStartButton.setAttribute("style", "visibility: hidden;");
+
+    // Display the first 2 answer selections options
     btnSelectionA.textContent = optionsArray[0];
     btnSelectionB.textContent = optionsArray[1];
-    if (isTrueFalse === false) {
+
+    // Check if the current question is a True/False
+    if (isTrueFalse === false) { // If it's normal
+        // Set the other 2 answer selections options
         btnSelectionC.textContent = optionsArray[2];
         btnSelectionD.textContent = optionsArray[3];
+
+        // And make sure they are visible
         btnSelectionC.setAttribute("style", "visibility:visible;");
         btnSelectionD.setAttribute("style", "visibility:visible;");
-    } else {
+    } else { // If it is a True/False
+        // Hide the other 2 buttons
         btnSelectionC.setAttribute("style", "visibility:hidden;");
         btnSelectionD.setAttribute("style", "visibility:hidden;");
     }
@@ -124,12 +108,9 @@ function renderQuestionAndAnswer(isTrueFalse) {
     // Print results to screen
     // Call Render HighScore
 
-/* LISTEN */
-// STEP 2c
-// Function: Listen for user input ( PARAM element, isTrueFalse ) 
+// When an option button is clicked, check which was selected and return
+// string return type ==> returns "A", "B", "C", or "D"
 function parseUserInput(element, isTrueFalse) {
-    // string return type ==> returns "A", "B", "C", or "D"
-
     // Check which button was clicked
     if (isTrueFalse === true) {
         if (element === btnSelectionA) {
@@ -163,37 +144,55 @@ function gameLoop() {
     var countDownInterval = setInterval(function() {
         timeLeft--;
 
-        if (questionIndex < numQuestions) {
-            // Sanity check
-            // console.log(questionIndex);
-            // console.log(numQuestions);
+        // Check if we're done
+        if (questionIndex < numQuestions) { // If we're not done
 
+            /* Sanity check
+            // console.log(questionIndex);
+            // console.log(numQuestions);*/
+
+            // Render the next question, passing in if it is
+            // a True/False question
             renderQuestionAndAnswer(currentQuestion["isTrueFalse"]);
 
             if (userSelectedAnswer != "") { // If the user has answered
 
-                if (userSelectedAnswer == currentQuestion["answer"]) {
-                    // Correct!
-                    numCorrect++;
-                    clearScreen();
-                    questionIndex++;
+                if (userSelectedAnswer == currentQuestion["answer"]) { // Correct!
+                    playerScore += 10; // Add 10 to score
+                    clearScreen(); // Clear the Screen
+
+                    // Go to next question
+                    questionIndex++; 
                     currentQuestion = listOfQuestions[questionIndex]
+                    
+                    // Reset userSelectedAnswer
                     userSelectedAnswer = "";
-                    if (questionIndex < numQuestions) {
+
+                    // Check if we're done
+                    // NOTE: It looks like I'm repeating myself here,
+                    // and I am, but it's so that the screen refreshes
+                    // immediately after the player selects an option
+                    if (questionIndex < numQuestions) { // If we're not done
+                        // Render the next question, passing in if it is
+                        // a True/False question
                         renderQuestionAndAnswer(currentQuestion["isTrueFalse"]);
-                    } else {
-                        // Render results
+                    } else { // If we are done
+                        clearScreen(); // Clear the screen
+                        playerScore += timeLeft; // Add remaining time to the score!
+                        // Call renderResults()
+                        clearInterval(countDownInterval);
                     }
-                } else {
-                    // Incorrect!
-                    timeLeft = timeLeft - 10;
-                    userSelectedAnswer = "";
+                } else { // Incorrect!
+                    timeLeft = timeLeft - 10; // Subtract time
+                    userSelectedAnswer = ""; // Reset userSelectedAnswer
                 }
             }
         }
 
-        if(timeLeft <= 0) {
-            clearInterval(countDownInterval);
+        if(timeLeft <= 0) { // If we are out of time!
+            clearScreen(); // Clear the screen
+            // Call renderResults()
+            clearInterval(countDownInterval); // Exit the timer
         }
     }, 1000);
 
@@ -204,7 +203,6 @@ function gameLoop() {
 
 /* FUNCTION CALLS */
 init();
-renderWelcomeScreen();
 
 /* EVENTS */
 // STEP 1
